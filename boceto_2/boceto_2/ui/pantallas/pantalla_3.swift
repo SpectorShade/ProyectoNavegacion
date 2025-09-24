93211164a6a965c0c38c0fff171b16e9cb29fc9f
Jadzia Eleonore <raccoonevef@gmail.com>
2025-09-24 12:41:56 -0600

    [AGREGA] opciones a la pantalla de opciones dentro de la app

--- boceto_2/boceto_2/ui/pantallas/pantalla_3.swift
+++ boceto_2/boceto_2/ui/pantallas/pantalla_3.swift
@@ -7,14 +7,46 @@
 
 import SwiftUI
 
+enum SaboresPalomitas: String, CaseIterable, Identifiable{
+    case Mantequilla
+    case Natural
+    case QuesoChedar
+    case Doritos
+    case Proteina
+    
+    var id: Self { self }
+}
+
+
 struct PantallaOpciones: View {
-    @State var valor_slider = 50.0
+    @State var quiere_palomitas: Bool = false
+    @State var sabor_de_palomitas: SaboresPalomitas = .Mantequilla
     
     var body: some View {
         ScrollView{
             VStack{
-                Text("Hola mundo el valor del slider es: \(valor_slider)")
-                Slider(value: $valor_slider, in: 0...100)
+                Text("Formulairo para calcular la cantida de palomitas")
+                
+                HStack{
+                    Spacer()
+                    Text("¿Quieres palomitas?")
+                    Spacer()
+                    CajaSeleccionada(checado: $quiere_palomitas, tamaño: 25)
+                    Spacer()
+                }
+                
+                HStack{
+                    Text("¿Cuál sabor?")
+                    
+                    Picker(selection: $sabor_de_palomitas, label: Text("Sabor de palomitas")) {
+                        Text("Mantequilla").tag(SaboresPalomitas.Mantequilla)
+                        Text("Naturales").tag(SaboresPalomitas.Natural)
+                    }
+                    
+                }
+                
+                /// Text("Hola mundo el valor del slider es: \(valor_slider)")
+                /// Slider(value: $valor_slider, in: 0...100)
                 
             }
         }
