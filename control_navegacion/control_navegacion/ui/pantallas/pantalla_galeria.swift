d96e9832f7964ceac2de28e1797ebf60d05d8fa9
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-10-17 23:44:13 -0600

    [actualiza] pantalla_galeria.swift

--- control_navegacion/control_navegacion/ui/pantallas/pantalla_galeria.swift
+++ control_navegacion/control_navegacion/ui/pantallas/pantalla_galeria.swift
@@ -4,15 +4,33 @@
 //
 //  Created by Jadzia Gallegos on 29/09/25.
 //
-import SwiftUI
-
 struct PantallaGaleria: View {
     var body: some View {
-        Text("Pantalla de galeria")
-        NavigationLink{
-            PantallaInicio()
-        } label: {
-            Text("Ir a inicio")
+        ZStack {
+            Color.black.ignoresSafeArea() // Fondo negro
+            
+            VStack(spacing: 20) {
+                Text("Pantalla de galería")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.green) // Acento verde
+                
+                NavigationLink{
+                    PantallaInicio()
+                } label: {
+                    Text("Ir a inicio")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.green.opacity(0.3))
+                        .cornerRadius(8)
+                }
+            }
+            .padding()
         }
     }
+}
+
+#Preview {
+    PantallaGaleria()
+        .environment(ControladorGeneral())
 }
